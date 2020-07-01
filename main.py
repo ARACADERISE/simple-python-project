fbe33f1be801e929faa22842dc5b93ee8dab7c1c
GetStuffDone <t.targetyt@gmail.com>
2020-07-01 20:29:33 +0000

    eh

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import os, json
 class GatherFiles:
     def __init__(self,filename):
         self.filename = filename
+        self.BitContents = []
+        self.ArraySize = 0
         self.filecontents = ""
         self.HasMadeBit = False
+        self.HasReleasedMemory = False
+        self.HasRestoredFile = False
         self.fileSize = 0
     
     def RepFile(self):
@@ -21,8 +25,7 @@ class GatherFiles:
         if self.filecontents != "":
             for i in range(len(self.filecontents)):
                 self.fileSize += 8
-            self.fileSize /= 8
-            self.fileSize = int(self.fileSize)
+            self.fileSize = int(self.fileSize/8)
     
     def MakeBit(self,convert=False,end=' '):
         if self.filecontents != "":
@@ -37,17 +40,59 @@ class GatherFiles:
                     write_file.write(chr(ord(i)))
                     write_file.write(end)
                     # print(ord(i),"\t",chr(ord(i)))
+                self.BitContents.append(ord(i))
+                self.ArraySize+=1
             write_file.close()
+
             self.HasMadeBit = True
     
     def ReleaseMemory(self):
         """Opens and deletes all file information"""
-        file_info = open(self.filename,"rw")
-        file_info.write("")
-        file_info.close()
+        if self.filecontents != "":
+            with open(self.filename,"w") as f:
+                f.write("")
+                f.close()
+            self.fileSize = int(0)
+            self.HasReleasedMemory = True
+    
+    def RestoreFile(self):
+        if self.filecontents != "":
+            with open(self.filename,"w") as f:
+                for i in self.BitContents:
+                    f.write(chr(i))
+                f.close()
+            
+            # Getting size again..
+            for i in range(len(self.filecontents)):
+                self.fileSize += 8
+            self.fileSize = int(self.fileSize/8)
+            self.HasRestoredFile = True
+
+    def Save(self):
+        DATA = {
+            "filename": self.filename,
+            "size": {
+                "bytes":self.fileSize
+            },
+            "MEMORY":{
+                "has_released": self.HasReleasedMemory,
+                "has_restored": self.HasRestoredFile   
+            }
+        }
+        with open("info.json","w") as f:
+            f.write(json.dumps(
+                DATA,
+                indent=2,
+                sort_keys=True
+            ))
+            f.close()
+    
 
 
 gf = GatherFiles("main.py")
 gf.RepFile()
 gf.SetSize()
 gf.MakeBit(convert=True)
+gf.ReleaseMemory()
+gf.RestoreFile()
+gf.Save()
